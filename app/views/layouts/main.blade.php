0ad323a16dd95caceed2b3034daab05dff895891
Paul Tela <paul@paultela.com>
2014-02-04 18:48:01 -0500

    Added flash messages

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -45,7 +45,12 @@
     </div>
 
     <div class="container">
-		
+    @if (Session::has('error_message'))
+      <div class="alert alert-danger">{{{ Session::get('error_message') }}}</div>
+    @endif
+    @if (Session::has('message'))
+      <div class="alert alert-success">{{{ Session::get('message') }}}</div>
+    @endif
 		@yield('content')
 	
     </div><!-- /.container -->
