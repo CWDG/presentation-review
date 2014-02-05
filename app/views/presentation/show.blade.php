3af781ef363c8b3bcec24308b51c35f738df84da
Paul Tela <paul@paultela.com>
2014-02-04 19:50:04 -0500

    Added shorthand /p/slug redirect

--- app/views/presentation/show.blade.php
+++ app/views/presentation/show.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 <h1>{{{ $presentation->name }}}</h1>
-
+<p class="lead">{{ link_to('/p/' . $presentation->slug, url('/p/' . $presentation->slug)) }}</p>
 <p>{{{ $presentation->additional_info }}}</p>
 @stop
