74d4034d3c6aea60c47cc34a86a5985d4a2ece3f
Paul Tela <paul@paultela.com>
2014-02-17 14:33:13 -0500

    Removed script tags from page-js section

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -62,9 +62,7 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
-		<script type="text/JavaScript">
-			@yield('page-js')
-		</script>
+		@yield('page-js')
   </body>
 </html>
 
